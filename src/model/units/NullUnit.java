ec0a8cb2d1dec90b26dadcd0f7d97fe060de417e
sebaaa <saguilerav98@gmail.com>
2019-11-20 00:09:35 -0300

    meh

--- src/model/units/NullUnit.java
+++ src/model/units/NullUnit.java
@@ -3,9 +3,17 @@ package model.units;
 import model.items.IEquipableItem;
 import model.map.InvalidLocation;
 
+/**
+ * This class represents a <i>NullUnit</i> type unit.
+ * <p>
+ * This type of Unit It literally does nothing and can't be added to a tactician unit list
+ *
+ * @author Sebastián Aguilera Valenzuela
+ * @since 2.5
+ */
 public class NullUnit extends AbstractUnit {
     /**
-     * Creates a Null Unit. It literally does nothing and can't be added to a tactician unit list
+     * Creates a Null Unit.
      */
     public NullUnit() {
         super(-1, -1, new InvalidLocation(), 0);
